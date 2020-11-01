b0f81c6074959e20fdefb3f3592d96167fb91648
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-11-01 20:01:42 +0200

    Initial commit

---
+++ Lab5.3.cpp
@@ -0,0 +1,53 @@
+﻿
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+double f(const double x);
+
+int main()
+{
+	double tp, tk, n, t;
+	cout << "tp = "; cin >> tp;
+	cout << "tk = "; cin >> tk;
+	cout << "n = "; cin >> n;
+
+	double tg = (tk - tp) / n;
+
+	t = tp;
+
+	while (t <= tk) {
+		double z = f(t * t) + 2 * f(2 * t + 1);
+
+		cout << t << "  " << z << endl;
+
+		t += tg;
+	}
+
+	cin.get();
+	system("pause");
+	return 0;
+}
+
+double f(const double x) {
+	if (abs(x) >= 1)
+		return (sin(x) + 1) / (sin(x) + cos(x));
+	else {
+		double S = 0;
+		int i = 0;
+		double a = 1;
+		S = a;
+		do {
+			i++;
+			double R = (x * x) / (2.0 * i * (2.0 * i + 1));
+			a *= R;
+			S += a;
+		} while (i < 8);
+		return S;
+	}
+}
+		
+
+
